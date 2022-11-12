b968c1a544cf8547f542232f3198d0f488b6115e
iHyunWoo <jrwedo@naver.com>
2022-11-13 04:08:08 +0900

    add: mainVC임시

--- client/Team6/View/JoinCompetition/JoinCompetitionViewController.swift
+++ client/Team6/View/JoinCompetition/JoinCompetitionViewController.swift
@@ -14,7 +14,9 @@ class JoinCompetitionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+		
+		
+		
         // Do any additional setup after loading the view.
     }
     
