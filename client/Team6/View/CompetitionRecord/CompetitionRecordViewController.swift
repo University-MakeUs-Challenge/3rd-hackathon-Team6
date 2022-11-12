0be1e120666ffaef1d9e8570bc72a55a5ab7901c
jinnxyoung <wlsdud7722@naver.com>
2022-11-13 07:01:12 +0900

    Add first commit

--- client/Team6/View/CompetitionRecord/CompetitionRecordViewController.swift
+++ client/Team6/View/CompetitionRecord/CompetitionRecordViewController.swift
@@ -16,14 +16,49 @@ class CompetitionRecordViewController: UIViewController {
     static let className = "CompetitionRecordViewController"
 	static let identifier = "CompetitionRecordViewController"
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
 
     @IBAction func backButtonDidTap(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
     }
+    
+}
+
+extension CompetitionRecordViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CompetitionRecordCollectionViewCell", for: indexPath) as? CompetitionRecordCollectionViewCell else {return UICollectionViewCell()}
+        
+        switch indexPath.item {
+        case 0:
+            cell.categoryLabel.text = "GitHub"
+            cell.progressView.setProgress(0.75, animated: false)
+        case 1:
+            cell.categoryLabel.text = "백준"
+            cell.progressView.setProgress(0.25, animated: false)
+        case 2:
+            cell.categoryLabel.text = "Quiz"
+            cell.progressView.setProgress(0.5, animated: false)
+        case 3:
+            cell.categoryLabel.text = "기여도"
+            cell.progressView.setProgress(0.55, animated: false)
+        default:
+            break
+        }
+        
+        return cell
+    }
+    
     
 }
 
