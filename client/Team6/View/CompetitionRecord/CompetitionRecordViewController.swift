6c78a2f6202644c364ab039a1b72178fc5f093e2
jinnxyoung <wlsdud7722@naver.com>
2022-11-13 06:34:05 +0900

    Add first commit

--- client/Team6/View/CompetitionRecord/CompetitionRecordViewController.swift
+++ client/Team6/View/CompetitionRecord/CompetitionRecordViewController.swift
@@ -8,25 +8,22 @@
 import UIKit
 
 class CompetitionRecordViewController: UIViewController {
-	
-	static let className = "CompetitionRecordViewController"
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userSchoolLabel: UILabel!
+    @IBOutlet weak var userRankLabel: UILabel!
+    @IBOutlet weak var backButton: UIButton!
+    
+    static let className = "CompetitionRecordViewController"
 	static let identifier = "CompetitionRecordViewController"
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
 
-        // Do any additional setup after loading the view.
+    @IBAction func backButtonDidTap(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
