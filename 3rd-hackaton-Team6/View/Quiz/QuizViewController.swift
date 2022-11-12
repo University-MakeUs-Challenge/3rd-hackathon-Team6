4a5e5fa5ebe434631de6224ed56ce2af256316f2
iHyunWoo <jrwedo@naver.com>
2022-11-12 23:57:29 +0900

    add: storyboards

---
+++ 3rd-hackaton-Team6/View/Quiz/QuizViewController.swift
@@ -0,0 +1,32 @@
+//
+//  QuizViewController.swift
+//  3rd-hackaton-Team6
+//
+//  Created by 정현우 on 2022/11/12.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+	
+	static let className = "QuizViewController"
+	static let identifier = "QuizViewController"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
